ac04d8e68a8b1d1740b9eeacbb6fa1ab38a470bd
Prateek Vijay <113239008+pkvijay65@users.noreply.github.com>
2023-07-03 23:06:52 +0530

    implement circuit breaker

--- order-service/src/main/java/com/ecommerce/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/ecommerce/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.ecommerce.orderservice.controller;
 
 import com.ecommerce.orderservice.dto.OrderRequest;
 import com.ecommerce.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -20,8 +21,14 @@ public class OrderController {
   private final OrderService orderService;
 
   @PostMapping
+  @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
   public ResponseEntity<String> placeOrder(@RequestBody OrderRequest orderRequest) {
     log.info("Placing Order");
     return ResponseEntity.status(HttpStatus.CREATED).body(orderService.placeOrder(orderRequest));
+  }
+
+  public ResponseEntity<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+    log.info("Cannot Place Order, hence Executing Fallback logic");
+    return ResponseEntity.status(HttpStatus.GATEWAY_TIMEOUT).body("We are updating our inventory, please try after sometime");
   }
 }
